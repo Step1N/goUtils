410e787ec184a39bc126626238655635568935dd
Durgesh Kumar Gaur <durgesh.gaur@imaginea.com>
2017-01-10 14:40:03 +0530

    Fixed pop issue

--- stack/stack.go
+++ stack/stack.go
@@ -1,63 +1,43 @@
 package stack
 
-import "fmt"
 
 type Stack struct {
-	input []interface{}
-	count int
+	top  *Frame
+	size int
+}
+type Frame struct {
+	value interface{}
+	next  *Frame
 }
 
 func NewStack() *Stack {
-	s := newStack()
+	s := new(Stack)
 	return s
 }
 
-func newStack() *Stack {
-	in := make([]interface{}, 0)
-	return &Stack{input: in, count: -1}
+func (s *Stack) Length() int {
+	return s.size
 }
 
-func (s *Stack) Push(element interface{}) {
-	if s.count == -1 {
-		ns := newStack()
-		ns.input = append(ns.input, element)
-		s.input = ns.input
-		s.count = s.count + 1
-		return
-	}
-
-	s.input = append(s.input, element)
+func (s *Stack) IsEmpty() bool {
+	return s.size == 0
 }
 
-func (s *Stack) Pop() interface{} {
-	if len(s.input) == 0 {
-		s.count = -1
-		return -1
-	}
-
-	el := s.input[s.count]
-	s.input = s.input[s.count+1:]
-	return el
+func (s *Stack) Push(val interface{}) {
+	s.top = &Frame{val, s.top}
+	s.size++
 }
 
 func (s *Stack) Peek() interface{} {
-	if len(s.input) == 0 {
-		return -1
-	}
-
-	return s.input[s.count]
+	return s.top.value
 }
 
-func (s *Stack) Size() int{
-
-	return len(s.input)
-}
-
-func (s *Stack) Print() {
-	if s.count == -1 {
+func (s *Stack) Pop() (val interface{}) {
+	if s.size > 0 {
+		val, s.top = s.top.value, s.top.next
+		s.size--
 		return
 	}
-	for i := 0; i < len(s.input); i++ {
-		fmt.Printf("%v \t", s.input[i])
-	}
+
+	return -1
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,42 +1,34 @@
 package stack
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPushElementInStack(t *testing.T) {
-	s := newStack()
+	s := NewStack()
 	s.Push("a")
 	s.Push("b")
 	s.Push("d")
 	s.Push("e")
-	fmt.Println("Stack elements")
-	s.Print()
 }
 
 func TestPopElementInStack(t *testing.T) {
-	s := newStack()
+	s := NewStack()
 	s.Push("a")
 	s.Push("b")
 	s.Push("d")
 	s.Push("e")
 	s.Push("f")
-	s.Print()
 	s.Pop()
 	s.Pop()
 	s.Pop()
-	fmt.Println("Stack elements after removeing 3 elements")
-	s.Print()
 }
 
 func TestPeekElementInStack(t *testing.T) {
-	s := newStack()
+	s := NewStack()
 	s.Push("a")
 	s.Push("b")
 	s.Push("d")
 	s.Push("e")
 	s.Push("f")
-	s.Print()
-	fmt.Println("Stack peek element", s.Peek())
 }
